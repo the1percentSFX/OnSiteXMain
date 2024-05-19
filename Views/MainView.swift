b4f5bda9c9d78c4af2802255f3b05028e70e5fc2
Tristan Shelton <trishelton@corelogic.com>
2024-05-18 19:12:01 -0500

    create new branch add login and registration form views D20240518
    (cherry picked from commit 89a6719de2f6bc79e305cd3068bbaca37f907c35)

---
+++ Views/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  MainView.swift
+//  OnSiteX
+//
+//  Created by Tristan Shelton on 5/18/24.
+//
+
+import SwiftUI
+
+@main
+struct MainView: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+       
+    }
+}
+
+
+
